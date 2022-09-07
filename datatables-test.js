d45b864f4b97d685314b6041208ccb61f61a0a17
GeoCistern <85967200+GeoCistern@users.noreply.github.com>
2022-09-07 15:45:28 -0700

    Add files via upload

---
+++ datatables-test.js
@@ -0,0 +1,7 @@
+//import {DataTable} from "simple-datatables";
+//const table = new DataTable("table");
+
+
+require( 'datatables.net-dt' )();
+require( 'datatables.net-buttons-dt' )();
+require( 'datatables.net-searchpanes-dt' )();
